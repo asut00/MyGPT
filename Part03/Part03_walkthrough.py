6d3f761ef36f31f9100fefd16d64ea12326c1ef6
asut00 <alexandresuteau@gmail.com>
2025-09-19 11:21:19 +0200

    update p03

--- Part03/Part03_walkthrough.py
+++ Part03/Part03_walkthrough.py
@@ -59,5 +59,75 @@ query = inputs[1] # 2nd input token is the query
 context_vec_2 = torch.zeros(query.shape)
 for i,x_i in enumerate(inputs):
     context_vec_2 += attn_weights_2[i]*x_i
-
+print('-----')
+print('context_vec_2:')
 print(context_vec_2)
+
+######################################################################################
+################## Computing attention weights for all input tokens ##################
+######################################################################################
+
+# 1. Compute attention scores (simplified representation = dot product of query with every other x(i))
+# 2. Compute attention weights (simplified representation = normalized version of the attention scores (with softmax))
+# 3. Compute context vectors (simplified representation = sum of each x(i) * w(i) FOR EACH INPUT)
+# ---> then you multiply the input with its context vector ?
+
+# Apply previous **step 1** to all pairwise elements to compute the unnormalized attention score matrix:
+
+inputs = torch.tensor(
+  [[0.43, 0.15, 0.89], # Your     (x^1)
+   [0.55, 0.87, 0.66], # journey  (x^2)
+   [0.57, 0.85, 0.64], # starts   (x^3)
+   [0.22, 0.58, 0.33], # with     (x^4)
+   [0.77, 0.25, 0.10], # one      (x^5)
+   [0.05, 0.80, 0.55]] # step     (x^6)
+)
+
+attn_scores = torch.empty(6, 6)
+
+
+# For each input x_i we create a matrix of 6 attention_scores corresponding to the 6 inputs x_j
+for i, x_i in enumerate(inputs):
+    for j, x_j in enumerate(inputs):
+        attn_scores[i, j] = torch.dot(x_i, x_j)
+
+print('-----')
+print('attn_score:')
+print(attn_scores)
+
+# We can achieve the same as above more efficiently via matrix multiplication:
+attn_scores = inputs @ inputs.T
+print('-----')
+print('attn_score:')
+print(attn_scores)
+
+# Similar to step 2 previously, we normaliwe each row so that the values in each row sum to 1:
+attn_weights = torch.softmax(attn_scores, dim=1)
+print('-----')
+print('attn_weights:')
+print(attn_weights)
+
+# Verification
+row_2_sum = sum([0.1385, 0.2379, 0.2333, 0.1240, 0.1082, 0.1581])
+print('-----')
+print("Row 2 sum:", row_2_sum)
+print("All row sums:", attn_weights.sum(dim=-1))
+
+# Step 3 : compute all context vectors (simplified representation = (sum of each x(i) * w(i)) that we multiply with x_(i) FOR EACH INPUT)
+all_context_vecs = attn_weights @ inputs
+print('-----')
+print('all_context_vecs:')
+print(all_context_vecs)
+
+# As a for loop it would be :
+all_context_vecs = torch.zeros_like(inputs)  # (6,3)
+for i in range(attn_weights.shape[0]):   # pour chaque token i
+    context_vec = torch.zeros(inputs.shape[1])  # vecteur (3,)
+    for j in range(inputs.shape[0]):     # pour chaque token j
+        # print(f'attn_weights[{i}, {j}]: {attn_weights[i, j]}')
+        context_vec += attn_weights[i, j] * inputs[j]
+        # exit()
+    all_context_vecs[i] = context_vec
+
+print("Previous 2nd context vector:", context_vec_2)
+
