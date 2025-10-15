bba46055e37a22e2fa13a401d883a89ef419d102
asut00 <alexandresuteau@gmail.com>
2025-10-15 23:05:10 +0200

    start part 04

---
+++ Part04/Part04_walkthrough.py
@@ -0,0 +1,5 @@
+from importlib.metadata import version
+
+print("matplotlib version:", version("matplotlib"))
+print("torch version:", version("torch"))
+print("tiktoken version:", version("tiktoken"))
